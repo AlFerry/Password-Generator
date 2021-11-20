b5e71dc3288868e054dbfebb2bb165ab4ab089a8
Alyssa Ferry <alyssa.ferry96@gmail.com>
2021-11-19 21:03:54 -0800

    Populated arrays for different character types.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//var dclaration, create arrays of characters
+
+var special=["!", "@", "#", "$", "%", "^", "&", "*", "~", "?", "-", "_", "+", "=", "<", ">", "/", ",", "."];
+var lower=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var num=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,5 +17,6 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+
+// Add event listener to generate button, Nothing after this
 generateBtn.addEventListener("click", writePassword);
